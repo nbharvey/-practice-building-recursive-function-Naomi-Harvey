9568b0b90d3184a6d23df95e0559a39680d8010b
Naomi Harvey <naomi.b.harvey@gmail.com>
2025-04-09 16:39:15 -0500

    built a recursive function to reverse a string

---
+++ app.js
@@ -0,0 +1,9 @@
+function reverseString(str) {
+    return str.split("").reverse().join("");//split string into seperate substrings, reverse the order, joing the substrings back together
+}
+console.log(reverseString("hello")); //Output: olleh
+console.log(reverseString("recursion")); //Output: noisrucer
+console.log(reverseString("a")); //Output: a
+console.log(reverseString("coding is fun")); //Output: nuf si gnidoc
+
+
