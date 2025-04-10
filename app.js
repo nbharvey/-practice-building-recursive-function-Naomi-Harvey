6a0e1d026be056970060b6713a9f32d0cf3005d4
Naomi Harvey <naomi.b.harvey@gmail.com>
2025-04-10 10:28:32 -0500

    used recursion this time!!!

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-function reverseString(str) {
+
+/*function reverseString(str) {
     return str.split("").reverse().join("");//split string into seperate substrings, reverse the order, joing the substrings back together
 }
 console.log(reverseString("hello")); //Output: olleh
 console.log(reverseString("recursion")); //Output: noisrucer
-console.log(reverseString("a")); //Output: a
-console.log(reverseString("coding is fun")); //Output: nuf si gnidoc
+console.log(reverseString(" ")); //Output: a
+console.log("" + reverseString(" ") + ""); // Output: [ ]console.log(reverseString("coding is fun")); //Output: nuf si gnidoc
+*/
 
+NEW ATTEMPT WITH RECURSION
+function reverseString(str) {
+    if (str.length === 1 || 0) {
+        return str;
+    }
+    return str.charAt(str.length - 1) + reverseString(str.slice(0, -1));
+}
+
+console.log(reverseString("hello")); //Output: olleh
+console.log("" + reverseString(" ") + ""); // Output: [ ]console.log(reverseString("coding is fun")); //Output: nuf si gnidoc
+console.log(reverseString("recursion")); //Output: noisrucer
 
